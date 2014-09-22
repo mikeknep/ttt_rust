26f87e44fcaf127f2e5b78ffb4c77a84b351e4a6
Mike Knepper <michaelrknepper@gmail.com>
2014-09-22 18:51:08 -0500

    Adds player struct to ttt::core
    Player has a Token and a decision-making function

--- ttt/core/mod.rs
+++ ttt/core/mod.rs
@@ -1,2 +1,3 @@
 pub mod board;
 pub mod gameplay_executor;
+pub mod player;
---
+++ ttt/core/player.rs
@@ -0,0 +1,34 @@
+use super::board::{Token, Board};
+
+pub struct Player {
+    pub token: Token,
+    decision_maker: fn(&Board) -> uint
+}
+
+impl Player {
+    pub fn new(token: Token, decision_maker: fn(&Board) -> uint) -> Player {
+        Player { token: token, decision_maker: decision_maker }
+    }
+
+    pub fn choose_next_move(&self, board: &Board) -> uint {
+        let dm = self.decision_maker;
+        dm(board)
+    }
+}
+
+
+
+
+
+#[test]
+fn choosing_next_move_returns_cell_index() {
+    use super::board::{O};
+    #[allow(unused_variable)]
+    fn mock_decision_maker(board: &Board) -> uint {
+        4u
+    }
+    let player: Player = Player::new(O, mock_decision_maker);
+    let board = Board::new();
+
+    assert!(player.choose_next_move(&board) == 4u);
+}
