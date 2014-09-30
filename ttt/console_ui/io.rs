632a18ddcf4a2358c840c36c9a7c14bb9e124a59
Mike Knepper <michaelrknepper@gmail.com>
2014-09-30 09:13:50 -0500

    Extracts stdin prompt fn from public consoleIO fns

--- ttt/console_ui/io.rs
+++ ttt/console_ui/io.rs
@@ -4,11 +4,10 @@ use super::super::core::{rules, simple_ai, unbeatable_ai};
 
 pub fn get_next_move(board: &Board, _token: Token) -> uint {
     println!("Where do you want to play?");
-    let mut reader = io::stdin();
     loop {
-        let input = reader.read_line().ok().expect("There was a problem reading your input.");
-        let input_num: Option<uint> = from_str(input.as_slice().trim());
-        match input_num {
+        let input_char = get_input().unwrap();
+        let input_digit = input_char.to_digit(9u);
+        match input_digit {
             Some(position) if rules::is_valid_position(position, board) => return position,
             _ => println!("That is not a valid position. Please select an open spot on the board as an integer.")
         };
@@ -18,29 +17,29 @@ pub fn get_next_move(board: &Board, _token: Token) -> uint {
 
 pub fn get_player_decision_maker(player_number: uint) -> fn(&Board, Token) -> uint {
     println!("What type of player is player {}? (h)uman, (e)asy computer, or (d)ifficult computer", player_number);
-    let mut reader = io::stdin();
     loop {
-        let input = reader.read_line().ok().expect("There was a problem reading your input.");
-        let input_char: &str = input.as_slice().trim();
-        match input_char {
-            "h" => return get_next_move,
-            "e" => return simple_ai::choose_first_available_cell,
-            "d" => return unbeatable_ai::choose_best_available_cell,
-            _ => println!("That is not a valid player type. Please enter \"h\" for human, \"e\" for easy computer, or \"d\" for difficult computer.")
+        match get_input().unwrap() {
+            'h' => return get_next_move,
+            'e' => return simple_ai::choose_first_available_cell,
+            'd' => return unbeatable_ai::choose_best_available_cell,
+             _  => println!("That is not a valid player type. Please enter \"h\" for human, \"e\" for easy computer, or \"d\" for difficult computer.")
         };
     }
 }
 
 pub fn get_play_again() -> bool {
     println!("Do you want to play again? (y)es / (n)o");
-    let mut reader = io::stdin();
     loop {
-        let input = reader.read_line().ok().expect("There was a problem reading your input.");
-        let input_char: &str = input.as_slice().trim();
-        match input_char {
-            "y" => return true,
-            "n" => return false,
-            _ => println!("That is not a valid response. Please enter \"y\" to play again or \"n\" to exit.")
+        match get_input().unwrap() {
+            'y' => return true,
+            'n' => return false,
+             _  => println!("That is not a valid response. Please enter \"y\" to play again or \"n\" to exit.")
         };
     }
+}
+
+fn get_input() -> Option<char> {
+    let mut reader = io::stdin();
+    let mut input = reader.read_line().ok().expect("There was a problem reading your input.");
+    input.shift_char()
 }
